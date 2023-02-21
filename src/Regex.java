3661491cfa902710f3308680d5d597cc87762e20
martin378rm <martinrivaldo112@gmail.com>
2023-02-21 10:31:28 +0700

    Tugas Pekan 4

--- src/Regex.java
+++ src/Regex.java
@@ -7,7 +7,6 @@ public class Regex {
 
         // Tugas Regex
         patternCheck(input);
-
     }
 
 
@@ -15,6 +14,7 @@ public class Regex {
         String emailPattern = "^[\\w-]+@([\\w-]+\\.)+[\\w-]{2,4}$";
         String datePattern1 = "\\d{2}-\\d{2}-\\d{4}";
         String datePattern2 = "^([A-Za-z]+),([A-Za-z]+),\\d{2}$";
+        String datePattern3 = "^([A-Za-z]+),\\s([A-Za-z]+),\\s\\d{2}$";
         String ipPattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
                            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
                            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
@@ -24,10 +24,12 @@ public class Regex {
             System.out.println(str + " Ini email");
         } else if (str.matches(datePattern1)){
             System.out.println(str + " Ini date pattern1");
-        } else if (str.matches(datePattern2)){
+        } else if (str.matches(datePattern2) || str.matches(datePattern3)){
             System.out.println(str + " Ini date pattern2");
         } else if (str.matches(ipPattern)) {
             System.out.println(str + " Ini pattern ip");
+        } else {
+            System.out.println(str + " pattern tidak ada yang cocok");
         }
     }
 }
